8733d6cc962e1858efe4307ec0577effac5d16eb
Anwesh43 <anweshthecool0@gmail.com>
2020-11-19 22:28:38 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -29,3 +29,47 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawRectTrapzPath(context : CanvasRenderingContext2D, scale : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const sf4 : number = ScaleUtil.divideScale(sf, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        const tSize : number = Math.min(w, h) / tFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        DrawingUtil.drawLine(context, -size / 2, h / 2, -size / 2, h / 2 - (h / 2 - tSize) * sf1)
+        DrawingUtil.drawLine(context, -size / 2, -tSize, -size / 2 + size * sf2, -tSize + tSize * sf2)
+        DrawingUtil.drawLine(context, size / 2, 0, size / 2, h * 0.5 * sf3)
+        context.save()
+        context.beginPath()
+        context.moveTo(-size / 2, h / 2)
+        context.lineTo(-size / 2, -tSize)
+        context.lineTo(size / 2, 0)
+        context.lineTo(size / 2, h / 2)
+        context.lineTo(-size / 2, h / 2)
+        context.clip()
+        context.fillRect( -size / 2, h / 2 - (h / 2 + tSize) * sf4, size, (h / 2 + tSize) * sf4)
+        context.restore()
+        context.restore()
+    }
+    
+    static drawRTFNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawRectTrapzPath(context, scale)
+    } 
+}
