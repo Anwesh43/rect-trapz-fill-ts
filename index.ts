37f6e699864ae28e2e15801162e9f0bcd6152cfd
Anwesh43 <anweshthecool0@gmail.com>
2020-11-19 22:55:39 +0530

    adding RectTrapzFill class

--- index.ts
+++ index.ts
@@ -188,3 +188,26 @@ class RTFNode {
         return this 
     }
 }
+
+class RectTrapzFill {
+
+    curr : RTFNode = new RTFNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
